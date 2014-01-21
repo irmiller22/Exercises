dbb0e3af20793d5e0ef072bb86b55d46c1d2ca38
irmiller22 <irmiller22@gmail.com>
2014-01-21 14:09:16 -0500

    finished tests, refactored code

--- project_euler/CubicPermutations.rb
+++ project_euler/CubicPermutations.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require 'test/unit'
 # The cube, 4106325 (345^3) can be permuted to produce two other cubes: 56623104 (384^3) and 66430125 (405^3).  In fact, 4106325 is the smallest cube which has exactly three permutations of its digits which are also cube.
 
@@ -35,9 +34,11 @@ end
 
 class CubicPermutationTest < Test::Unit::TestCase
 	def test_perm
+		assert_equal [41063625, 56623104, 66430125], CubicPermutation.new(3).final
+		assert_equal 41063625, CubicPermutation.new(3).final.first
+		assert_equal (345 ** 3), CubicPermutation.new(3).final.first
 		assert_equal 5, CubicPermutation.new(5).final.count
 		assert_equal (5027 ** 3), CubicPermutation.new(5).final.first
 		assert_equal 127035954683, CubicPermutation.new(5).final.first
-		
 	end
 end
